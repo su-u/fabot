26379016e580ec3693e4c69b61fc3d6ea5946c51
su-u <aaaiiiuuu35@gmail.com>
2017-07-14 12:14:11 +0900

    Add files via upload

---
+++ fabot - コピー.php
@@ -0,0 +1,29 @@
+<?php
+    require "/twitteroauth/autoload.php";
+    use Abraham\TwitterOAuth\TwitterOAuth;
+
+    $consumerKey = "";
+    $consumerSecret = "";
+    $accessToken = "";
+    $accessTokenSecret = "";
+
+    $connection = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
+
+	$id = '3242839746';
+	$file = 'log.log';
+	$log = file_get_contents($file);
+	//TL取得
+	$tl_1 = $connection->get('statuses/user_timeline', ['user_id' => $id, 'count' => 5]);
+	$tl_1 = json_decode(json_encode($tl_1), true);	
+	$tl_time = $tl_1[0]['created_at'];
+	//echo $tl_time;
+
+	// ツイートにいいね
+	if ($log != $tl_time){
+		foreach ($tl_1 as $t) {
+		    $connection->post('favorites/create', ['id' => $t['id']]);
+		    sleep(1);
+		}
+		//echo $tl_time;
+		file_put_contents($file, $tl_time);
+	}
