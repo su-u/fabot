891ebabeeb48c70b5e57a7b14995106d0f95d632
su-u <aaaiiiuuu35@gmail.com>
2017-07-14 12:15:48 +0900

    Update fabot.php

--- fabot.php
+++ fabot.php
@@ -8,17 +8,18 @@
     $accessTokenSecret = "";
 
     $connection = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
-
-	$id = '3242839746';
+	
+	//search id
+	$id = '';
 	$file = 'log.log';
 	$log = file_get_contents($file);
-	//TLŽæ“¾
+	//TL
 	$tl_1 = $connection->get('statuses/user_timeline', ['user_id' => $id, 'count' => 5]);
 	$tl_1 = json_decode(json_encode($tl_1), true);	
 	$tl_time = $tl_1[0]['created_at'];
 	//echo $tl_time;
 
-	// ƒcƒC[ƒg‚É‚¢‚¢‚Ë
+	//iine!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 	if ($log != $tl_time){
 		foreach ($tl_1 as $t) {
 		    $connection->post('favorites/create', ['id' => $t['id']]);
